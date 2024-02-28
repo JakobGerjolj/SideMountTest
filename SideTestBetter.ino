f782b51f88f12378867c441c45ae5b76724213bb
Jakob <jakob@test.si>
2024-02-28 14:08:39 +0100

    COMPLETE AnalogPins

--- SideTestBetter.ino
+++ SideTestBetter.ino
@@ -1,15 +1,36 @@
-#include "lib/AnalogPins.h"
+#include "AnalogPins.h"
 
-AnalogPins apins;
+
+bool s1=false;
+
 
 void setup() {
-
-
-
-  Serial.begin(9600);
+  Serial.begin(115200);
 }
 
-void loop() {
 
+
+
+void loop() {
+  Serial.print("-------------------------------");
+  Serial.println();
+  Serial.print("ANALOG 33: ");
+  Serial.print(AnalogPins::is3_3OK());
+  Serial.println();
+
+  Serial.print("ANALOG 5: ");
+  Serial.print(AnalogPins::is5OK());
+  Serial.println();
+
+  Serial.print("ANALOG 12: ");
+  Serial.print(AnalogPins::is12OK());
+  Serial.println();
+
+  Serial.print("ANALOG 44: ");
+  Serial.print(AnalogPins::is44OK());
+  Serial.println("");
+ Serial.print("-------------------------------");
+
+  delay(1000);
 
 }
