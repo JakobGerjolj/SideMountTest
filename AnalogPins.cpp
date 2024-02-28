a082ab7367be9797e4a8b8482842d130b1a9fade
Jakob <jakob@test.si>
2024-02-28 14:16:35 +0100

    COMPLETED Analog pins

---
+++ AnalogPins.cpp
@@ -0,0 +1,38 @@
+#include "AnalogPins.h"
+
+
+
+bool AnalogPins::is3_3OK(){
+    if(analogRead(Pin3_3) < Val3_3 + napaka && analogRead(Pin3_3)  > Val3_3 - napaka)
+        return true;
+    else return false;
+
+}
+
+bool AnalogPins::is5OK(){
+    if(analogRead(Pin5)  < Val5 + napaka && analogRead(Pin5)  > Val5 - napaka)
+        return true;
+    else return false;
+
+}
+
+bool AnalogPins::is12OK(){
+    if(analogRead(Pin12)  < Val12 + napaka && analogRead(Pin12)  > Val12 - napaka)
+        return true;
+    else return false;
+
+}
+
+bool AnalogPins::is44OK(){
+    if(analogRead(Pin4)  < Val44 + napaka && analogRead(Pin4) > Val44 - napaka)
+        return true;
+    else return false;
+}
+
+float AnalogPins::GetValueFromPin(int pin)
+{
+
+    return analogRead(pin) * (5.0 / 1023.0);
+
+}
+
